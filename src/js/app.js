5c8111349fc61d5c72dfa9d1ccb47bff607c6d31
Nikita-2406 <89524208686@mail.ru>
2024-09-04 21:26:26 +0300

    1.0.0

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import { Popover } from "./popover/popover";
+
+const popover = new Popover(document.querySelector(".container"))
+
+popover.bindtoDOM()
---
+++ src/js/popover/popover.js
@@ -0,0 +1,27 @@
+export class Popover {
+  constructor (parentElem) {
+    this.parentElem = parentElem
+    this.bindtoDOM = this.bindtoDOM.bind(this)
+  }
+
+  bindtoDOM () {
+    const btn = document.createElement("button")
+    btn.classList.add("clicker_button")
+    btn.textContent = "CLICK HERE"
+
+    const popover = document.createElement('div')
+    popover.classList.add("popover")
+    popover.classList.add("unvisible")
+    popover.innerHTML = "<h3>Title</h3><p>text popover</p>"
+
+    this.parentElem.appendChild(btn)
+    this.parentElem.appendChild(popover)
+
+
+    btn.addEventListener("click", () => {
+      popover.classList.toggle("unvisible")
+      console.log("235423534 2wergwer")
+    })
+  }
+
+}
